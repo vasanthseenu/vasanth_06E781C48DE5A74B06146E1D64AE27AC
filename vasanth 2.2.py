6424500376e071dec892763f8c622bf8f74c88a9
vasanthseenu <147616769+vasanthseenu@users.noreply.github.com>
2023-10-11 17:51:52 +0530

    Add files via upload

---
+++ vasanth 2.2.py
@@ -0,0 +1,20 @@
+#implementation of the player class
+class Player:
+    def play(self):
+        print("The player is playing cricket.")
+
+class Batsman(Player):
+    def play(self):
+        print("The batsman is batting.")
+
+class Bowler(Player):
+    def play(self):
+        print("The bowler is bowling.")
+
+# Create objects of Batsman and Bowler classes and call the play() method for each object
+if __name__ == "__main__":
+    batsman = Batsman()
+    bowler = Bowler()
+
+    batsman.play()
+    bowler.play()
